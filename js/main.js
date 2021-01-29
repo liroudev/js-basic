ddcca18993a0000aabeee586c73ffd1e5f1a4d70
liroudev <liroudev@pm.me>
2021-01-29 19:30:41 -0300

    add js file with examples

---
+++ js/main.js
@@ -0,0 +1,42 @@
+var frutas = [{nome:"Maça",cor:"vermelha"},{nome:"Uva",cor:"Roxa"}];
+console.log(frutas);
+console.log(frutas[0]);
+console.log(frutas[1].nome);
+
+
+// var fruta = {nome:"maçã", cor:"vermelha"};
+// console.log(fruta);
+
+
+
+// var lista = ["maçã","pêra","laranja"];
+
+// console.log(lista[1]);
+// lista.push("uva");
+// console.log(lista);
+// console.log("Quantidade de elementos: " + lista.length);
+// lista.pop();
+// console.log(lista);
+// console.log("Quantidade de elementos: " + lista.length);
+// console.log(lista.reverse());
+// console.log(lista.toString());
+// console.log(lista.join(" | "));
+
+
+
+// var nome ='Filipe André';
+// var n1 = 15;
+// var n2 = 20;
+// var frase = "japão é o melhor time do mundo";
+// //alert("Bem Vindo " + nome + " você tem " + idade + " anos") ;
+
+
+
+// console.log(nome);
+// console.log(n1);
+// console.log(n2);
+// console.log(n1 * n2);
+// console.log(frase);
+// console.log(frase.toLocaleUpperCase());
+// console.log(frase.replace("japão","Brasil"));
+
