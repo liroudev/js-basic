dbcce3c997c043aaaec9c4da32b6b5f7d286bced
liroudev <liroudev@pm.me>
2021-01-30 14:08:22 -0300

    using functions

--- js/main.js
+++ js/main.js
@@ -1,7 +1,59 @@
-var frutas = [{nome:"Maça",cor:"vermelha"},{nome:"Uva",cor:"Roxa"}];
-console.log(frutas);
-console.log(frutas[0]);
-console.log(frutas[1].nome);
+function soma(n1,n2){
+    return n1 + n2;
+}
+
+alert(soma(5,10));
+
+function validaIdade(idade){
+    var validar;
+    if(idade >=18){
+        validar = true;
+    }else {
+        validar = false;
+    }
+    return validar;
+}
+
+var idade = prompt("Informe a sua idade");
+alert(validaIdade(idade));
+
+
+// var d = new Date();
+// alert(d.toDateString());
+// alert(d.getHours());
+// alert(d.getMonth()+1);
+
+
+// for(count = 1; count <= 5; count++){
+//     alert(count);
+// }
+
+
+// var count = 0;
+
+// while (count < 5){
+//     console.log(count);
+//     count++;
+// }
+
+
+
+// var idade = prompt("Qual sua idade ?");
+
+// if(idade >= 18){
+//     alert("Maior de idade!");
+// }else {
+//     alert("Menor de idade! Quer procurar besteira né ?!");
+// }
+
+
+
+
+
+// var frutas = [{nome:"Maça",cor:"vermelha"},{nome:"Uva",cor:"Roxa"}];
+// console.log(frutas);
+// console.log(frutas[0]);
+// console.log(frutas[1].nome);
 
 
 // var fruta = {nome:"maçã", cor:"vermelha"};
